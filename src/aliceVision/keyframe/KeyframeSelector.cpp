2e5b632c7fd6e488f6d03c3da5fee2b63c574cd3
Candice Bentéjac <candice.bentejac@gmail.com>
2023-01-31 19:51:49 +0100

    KeyframeSelection: Add support for several output extension files
    Keyframes may be written as JPG, PNG or EXR files. If the EXR format is
    selected, the storage data type can be specified as well.

--- src/aliceVision/keyframe/KeyframeSelector.cpp
+++ src/aliceVision/keyframe/KeyframeSelector.cpp
@@ -5,7 +5,6 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "KeyframeSelector.hpp"
-#include <aliceVision/image/all.hpp>
 #include <aliceVision/sensorDB/parseDatabase.hpp>
 #include <aliceVision/system/Logger.hpp>
 
@@ -370,7 +369,9 @@ bool KeyframeSelector::computeScores(const std::size_t rescaledWidth, const std:
 
 bool KeyframeSelector::writeSelection(const std::vector<std::string>& brands,
                                       const std::vector<std::string>& models,
-                                      const std::vector<float>& mmFocals) const
+                                      const std::vector<float>& mmFocals,
+                                      const std::string& outputExtension,
+                                      const image::EStorageDataType storageDataType) const
 {
     image::Image<image::RGBColor> image;
     camera::PinholeRadialK3 queryIntrinsics;
@@ -422,7 +423,7 @@ bool KeyframeSelector::writeSelection(const std::vector<std::string>& brands,
 
             fs::path folder = _outputFolder;
             std::ostringstream filenameSS;
-            filenameSS << std::setw(5) << std::setfill('0') << pos << ".exr";
+            filenameSS << std::setw(5) << std::setfill('0') << pos << "." << outputExtension;
             const auto filepath = (processedOutputFolder / fs::path(filenameSS.str())).string();
 
             image::ImageWriteOptions options;
@@ -432,6 +433,10 @@ bool KeyframeSelector::writeSelection(const std::vector<std::string>& brands,
                 options.toColorSpace(image::EImageColorSpace::AUTO);
             } else {  // Otherwise, the frames have been read without any conversion, they should be written as such
                 options.toColorSpace(image::EImageColorSpace::NO_CONVERSION);
+            }
+
+            if (storageDataType != image::EStorageDataType::Undefined && outputExtension == "exr"){
+                options.storageDataType(storageDataType);
             }
 
             image::writeImage(filepath, image, options, metadata);
